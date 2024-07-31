ce07bb93ac3c73900d0e2a2b7cb7fbf927c4f775
ricardorosa <rjrosa@outlook.com>
2024-07-31 13:45:27 +0100

    commiting urls

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,20 +1,3 @@
-from .models import Product
-from .forms import ProductForm
 from django.shortcuts import render
-from django.views import View
-from django.views.generic import ListView, DetailView, UpdateView, DeleteView
-from django.urls import reverse_lazy
 
 # Create your views here.
-
-class ProductListView(ListView):
-    model = Product
-    template_name = 'myapp/index.html'
-    context_object_name = 'products'
-
-
-
-
-
-
-
