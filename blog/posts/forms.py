c90181a4a1350b2cf1cb0a6aad0578d2b19111a2
ricardorosa <rjrosa@outlook.com>
2024-07-31 13:16:11 +0100

    commiting ProductListView, and Product form

---
+++ blog/posts/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Product
+
+
+class ProductForm(forms.ModelForm):
+    """Form for creating and updating Product instances."""
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'image']
