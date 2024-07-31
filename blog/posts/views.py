be773ff1ebbe13c330c957acedf2e44671bfd5c8
ricardorosa <rjrosa@outlook.com>
2024-07-31 13:45:26 +0100

    Revert "commiting ProductUpdateView"
    This reverts commit 34f4802dffa12a2c534a8e6c3834c22a68be2940.

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -14,14 +14,10 @@ class ProductListView(ListView):
 
 class ProductDetailView(DetailView):
     model = Product
-    template_name = 'myapp/index2.html'
+    template_name = 'myapp/pDetails.html'
     context_object_name = 'product'
 
-class ProductUpdateView(UpdateView):
-    model = Product
-    form_class = ProductForm
-    template_name = 'myapp/edit.html'
-    success_url = reverse_lazy('product_list')
+
 
 
 
